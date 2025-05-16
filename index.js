d39721eca32e1415f953d04a29e7223c9f59422a
Andres Fernandez <andres94.dev@gmail.com>
2025-05-16 11:32:01 +0200

    creando metodos sin implementacion

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ const app = express()
 app.use(json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Acortador de enlaces' })
+// Prueba de funcionamiento
+app.get('/test', (req, res) => {
+  res.json({ message: 'Acortador de enlaces OK' })
+})
+
+// Recibe un ID como parametro y devuelve el link original correspondiente
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  res.json({ id: `${id}`, url: 'https://example.com/12ewr345/98745wer' })
+})
+
+// Recibe en el cuerpo de la petición una URL, la guarda en la bbdd y retorna el ID generado
+app.post('/', (req, res) => {
+  const { url } = req.body
+  console.log(url)
+  res.json({ id: 'a9e3y6' })
 })
 
 app.listen(PORT, () => {
