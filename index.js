ec7a524f05831852432db02980fa9453fa208da9
Andres Fernandez <andres94.dev@gmail.com>
2025-05-16 13:02:08 +0200

    implementacion para guardar url y para obtener id desde la base de datos

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express, { json } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { ModelLink } from './model.js'
 dotenv.config()
 
 const PORT = process.env.PORT ?? 1234
 
 const app = express()
 app.use(json())
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 
 // Prueba de funcionamiento
 app.get('/test', (req, res) => {
@@ -15,16 +18,19 @@ app.get('/test', (req, res) => {
 })
 
 // Recibe un ID como parametro y devuelve el link original correspondiente
-app.get('/:id', (req, res) => {
+app.get('/:id', async (req, res) => {
   const { id } = req.params
-  res.json({ id: `${id}`, url: 'https://example.com/12ewr345/98745wer' })
+  const url = await ModelLink.getUrlById(id)
+  if (url !== '') return res.json({ id: `${id}`, url })
+  res.status(404).json({ error: 'Resource not found' })
 })
 
 // Recibe en el cuerpo de la petición una URL, la guarda en la bbdd y retorna el ID generado
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   const { url } = req.body
-  console.log(url)
-  res.json({ id: 'a9e3y6' })
+  const id = await ModelLink.saveUrl({ url })
+  if (id !== '') return res.json({ id })
+  res.status(409).json({ error: 'Failed to save your record to the database' })
 })
 
 app.listen(PORT, () => {
--- model.js
+++ model.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import { createClient } from '@libsql/client'
+import { generateRandomId } from './utils.js'
 dotenv.config()
 
 const db = createClient({
@@ -7,4 +8,45 @@ const db = createClient({
   authToken: process.env.DB_TOKEN
 })
 
-await db.execute('CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY, url TEXT);')
+await db.execute('CREATE TABLE IF NOT EXISTS links (id VARCHAR(10) PRIMARY KEY UNIQUE, url TEXT UNIQUE);')
+
+export class ModelLink {
+  static async saveUrl ({ url }) {
+    // Verifica si existe ya la url en la BBD, de ser asi retorna el id existente
+    try {
+      const result = await db.execute({
+        sql: 'SELECT * FROM links WHERE url=?',
+        args: [url]
+      })
+      const { id } = result.rows[0]
+      return id
+    } catch {}
+
+    // Si no existe, genera un nuevo id y guarda la nueva url y retorna el nuevo id
+    // De generarse un id duplicado retorna un string vacío que se interpreta como un conflicto
+    const id = generateRandomId()
+    try {
+      await db.execute({
+        sql: 'INSERT INTO links (id, url) VALUES (:id, :url);',
+        args: { id, url }
+      })
+      return id
+    } catch {
+      return ''
+    }
+  }
+
+  static async getUrlById (id) {
+    // Verifica si existe una url con el id indicado. Si existe retorna la url correspondiente, de lo contrario retorna un string vacío
+    try {
+      const result = await db.execute({
+        sql: 'SELECT * FROM links WHERE id=?',
+        args: [id]
+      })
+      const { url } = result.rows[0]
+      return url
+    } catch {
+      return ''
+    }
+  }
+}
